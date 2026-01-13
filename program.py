db4713f8c7a3a68d9699ce4ed436d4cbafc1aaed
Sangameshwar <119867115+Sangamesh080@users.noreply.github.com>
2026-01-13 08:49:48 +0530

    V6

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 #addition of two numbers
 a=100
 b=200
-print("Sum of two numbers: ", (a+b))
+prin("Sum of two numbers: ", (a+b))
 
 #Multiplication of two numbers
 print("Multiplication of two numbers: ",(a*b))
